1dd422833a67880213689cc6593be9c6349b0154
Curtis Liu <curtiskliu@gmail.com>
2026-04-01 22:18:46 -0600

    added a little bit more login logic

--- projectSourceCode/public/js/app.js
+++ projectSourceCode/public/js/app.js
@@ -18,9 +18,19 @@ async function checkLogin() {
   const response = await fetch('/api/auth/session');
   const data = await response.json();
 
+  const loggedOutDiv = document.getElementById('loggedOut');
+  const loggedInDiv = document.getElementById('loggedIn');
+  const welcomeMessage = document.getElementById('welcomeMessage');
+
   if (data.loggedIn) {
     console.log('User is logged in', data.user);
+    loggedInDiv.style.display = 'block';
+    loggedOutDiv.style.display = 'none';
+    welcomeMessage.textContent = `Welcome, ${data.user.username}`;//Maybe put icon in top left
   } else {
     console.log('User is NOT logged in');
+    loggedInDiv.style.display = 'none';
+    loggedOutDiv.style.display = 'block';
   }
 }
+checkLogin();
