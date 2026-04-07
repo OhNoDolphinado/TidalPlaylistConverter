b0c97ec71b3a4d28257823729568a6bbdceeed6d
Alex Class <aclass@sonic.net>
2026-04-07 15:02:07 -0600

    moved footer to js component

--- projectSourceCode/public/js/components/header.js
+++ projectSourceCode/public/js/components/header.js
@@ -1,15 +1,20 @@
 const header = `
-<header>
-  <h1>🎵 Tidal Playlist Converter</h1>
-  <nav>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/profile">Profile</a>
-    <form action="/logout" method="post">
-      <button type="submit">Logout</button>
-    </form>
+<nav class="navbar">
+    <a href="/" class="navbar-brand">
+      <span class="logo-icon">🎵</span>
+      TidalConverter
+    </a>
+    <ul class="navbar-nav" id="main-nav">
+      <li><a href="/">Home</a></li>
+      <li><a href="/about" class="active">About</a></li>
+      <li id="nav-login"><a href="/login" class="btn-nav-outline">Sign In</a></li>
+      <li id="nav-register"><a href="/register" class="btn-nav">Get Started</a></li>
+      <li id="nav-profile" class="hidden"><a href="/profile">Profile</a></li>
+      <li id="nav-logout" class="hidden">
+        <a href="#" onclick="handleLogout(event)" class="btn-nav-outline">Sign Out</a>
+      </li>
+    </ul>
   </nav>
-</header>
 `;
 
 document.currentScript.insertAdjacentHTML('beforebegin', header);
