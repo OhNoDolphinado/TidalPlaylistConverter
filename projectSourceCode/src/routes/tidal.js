e3fe1b7d93108b222d82cf039255f31c240cfc73
hvhvuu <63218769+hvhvuu@users.noreply.github.com>
2026-04-15 13:29:33 -0600

    mostly works

--- projectSourceCode/src/routes/tidal.js
+++ projectSourceCode/src/routes/tidal.js
@@ -15,4 +15,7 @@ router.get('/status', TidalController.status);
 // Disconnect Tidal from the current session
 router.post('/disconnect', TidalController.disconnect);
 
+// Convert a Spotify playlist to Tidal
+router.post('/convert', TidalController.convert);
+
 module.exports = router;
